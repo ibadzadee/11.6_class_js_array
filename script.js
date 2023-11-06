d9ad529f78238af3e0bede24d3318b0389f2729a
ibadzadeee <sara.ibadzade@gmail.com>
2023-11-06 17:29:23 +0400

    commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// let number = [1,2]
+// let numbers2 = number
+// numbers2[1] = 20
+// console.log(number)
+
+// let arr = [
+//     {   name:"sara",
+//         uni:"LDU",
+//         ixtisas: "RIM3"
+//     },
+//     {   name:"qwer",
+//         uni:"BDU",
+//         ixtisas: "RM"
+//     },
+//     {   name:"asdf",
+//         uni:"ADA",
+//         ixtisas: "cyberSecurity"
+//     },
+//     {
+//         name:"Samir",
+//         uni:"LDU",
+//         ixtisas: "RIM5"
+//     }
+// ]
+// console.log(arr)
+// alert(arr[3].name)
+    
+let person = {
+    name: "sara",
+    next: "samir"
+}
+
+let string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga amet praesentium accusamus repellendus pariatur quisquam dignissimos? Explicabo ducimus eligendi doloremque atque, ut voluptate. Ab dicta sequi maxime magni aspernatur similique"
+
+
+console.log(string.length)
+
+
